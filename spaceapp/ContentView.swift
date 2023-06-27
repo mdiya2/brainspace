14ef73277fdbc7f4e173f12c42657efb8071cb79
Diya Mishra <msdiyamishra@gmail.com>
2023-06-27 10:13:36 -0400

    background, logo, text

--- spaceapp/ContentView.swift
+++ spaceapp/ContentView.swift
@@ -11,16 +11,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "heart.fill")
-                .foregroundColor(Color.indigo)
-                .imageScale(.large)
-                
-            Text("BrainSpace")
-        }
-        .padding()
-    }
-}
+        ZStack {
+            VStack {
+                Image("image")
+                    .imageScale(.large)
+                Text("BrainSpace")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.738, green: 0.64, blue: 0.676))
+                    .multilineTextAlignment(.leading)
+            }//end of vstack
+            .padding()
+            .background(Color(red: 0.278, green: 0.339, blue: 0.342))
+        }//end of zstack
+    }//end of body
+}//end of struct
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
