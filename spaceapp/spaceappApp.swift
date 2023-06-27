964bdb967b7975019c34a26d4221015580a7f78a
Diya Mishra <msdiyamishra@gmail.com>
2023-06-27 09:32:04 -0400

    Initial Commit

---
+++ spaceapp/spaceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceappApp.swift
+//  spaceapp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct spaceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
