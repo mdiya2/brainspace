72b17c947064ffbc2dca8c08a333942d1e05b69e
Diya Mishra <msdiyamishra@gmail.com>
2023-06-28 13:18:30 -0400

    fixing bugs

---
+++ spaceapp/game.swift
@@ -0,0 +1,21 @@
+//
+//  game.swift
+//  spaceapp
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+struct game: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        var number = randint
+    }
+}
+
+struct game_Previews: PreviewProvider {
+    static var previews: some View {
+        game()
+    }
+}
