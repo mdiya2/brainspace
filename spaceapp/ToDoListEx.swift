477b02221c27d5f210f3e35e3e8b0c58953a5db5
Diya Mishra <msdiyamishra@gmail.com>
2023-06-29 10:32:09 -0400

    idk

---
+++ spaceapp/ToDoListEx.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListEx.swift
+//  spaceapp
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct ToDoListEx: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoListEx_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListEx()
+    }
+}
--- spaceapp/todolist.swift
+++ spaceapp/todolist.swift
@@ -1,46 +1,44 @@
 //
-//  todo-list.swift
+//  todolist.swift
 //  spaceapp
 //
-//  Created by Scholar on 6/27/23.
+//  Created by Scholar on 6/29/23.
 //
 
 import SwiftUI
 
+struct TodoItem: Identifiable {
+    let id = UUID()
+    var title: String
+}
+
 struct todolist: View {
-    @State private var isPresentingHome = false
+    @State private var todoItems: [TodoItem] = []
+    @State private var newItemTitle = ""
+
     var body: some View {
-        
-        
-        ZStack {
-            Color(red: 0.278, green: 0.339, blue: 0.342)
-                .edgesIgnoringSafeArea(.all)
-            VStack{
-                Text("Todo List")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0.737, green: 0.64, blue: 0.676))
-                    .multilineTextAlignment(.leading)
+        VStack {
+            HStack {
+                TextField("Add a new item", text: $newItemTitle)
                 Button(action: {
-                    isPresentingHome = true
+                    guard !newItemTitle.isEmpty else { return }
+                    let newItem = TodoItem(title: newItemTitle)
+                    todoItems.append(newItem)
+                    newItemTitle = ""
                 }) {
-                    Text("home")
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.738, green: 0.64, blue: 0.676))
+                    Text("Add")
                 }
-                .font(.title2)
-                .buttonStyle(.borderedProminent)
-                .tint(Color(red: 0.362, green: 0.217, blue: 0.298))
-                .sheet(isPresented: $isPresentingHome) {
-                    ContentView()
-                }
+            }.padding()
+
+            List(todoItems) { item in
+                Text(item.title)
             }
         }
     }
-    
-    struct todo_list_Previews: PreviewProvider {
-        static var previews: some View {
-            todolist()
-        }
+}
+
+struct todolist_Previews: PreviewProvider {
+    static var previews: some View {
+        todolist()
     }
 }
