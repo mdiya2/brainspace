fa4f5bc99b5587b1ed9f5b9c72c1e1b4be2b6c88
Diya Mishra <msdiyamishra@gmail.com>
2023-06-29 16:00:22 -0400

    edits

--- spaceapp/todolist.swift
+++ spaceapp/todolist.swift
@@ -3,61 +3,91 @@ import SwiftUI
 struct TodoItem: Identifiable {
     let id = UUID()
     var title: String
-    var isChecked: Bool = false
-    var isDeleted: Bool = false
+    var isCompleted: Bool = false
+}
+
+class TodoListViewModel: ObservableObject {
+    @Published var todoItems: [TodoItem] = []
+    
+    func addTodoItem(title: String) {
+        let newItem = TodoItem(title: title)
+        todoItems.append(newItem)
+    }
+    
+    func deleteTodoItem(indexSet: IndexSet) {
+        todoItems.remove(atOffsets: indexSet)
+    }
+    
+    func toggleTodoItemCompletion(todoItem: TodoItem) {
+        if let index = todoItems.firstIndex(where: { $0.id == todoItem.id }) {
+            todoItems[index].isCompleted.toggle()
+        }
+    }
 }
 
 struct todolist: View {
-    @State private var todoItems: [TodoItem] = []
+    @StateObject private var viewModel = TodoListViewModel()
     @State private var newItemTitle = ""
-
+    
     var body: some View {
-        VStack {
-            HStack {
-                TextField("Add a new item", text: $newItemTitle)
-                Button(action: {
-                    guard !newItemTitle.isEmpty else { return }
-                    let newItem = TodoItem(title: newItemTitle)
-                    todoItems.append(newItem)
-                    newItemTitle = ""
-                }) {
-                    Text("Add")
-                }
-            }.padding()
-
-            List {
-                ForEach(todoItems) { item in
-                    if !item.isDeleted {
-                        HStack {
-                            Button(action: {
-                                toggleItem(item)
-                            }) {
-                                Image(systemName: item.isChecked ? "checkmark.circle.fill" : "circle")
-                            }
-                            Text(item.title)
-                                .strikethrough(item.isChecked)
-                            Spacer()
-                            Button(action: {
-                                deleteItem(item)
-                            }) {
-                                Image(systemName: "trash")
+        NavigationView {
+            ZStack{
+                Color(red: 0.278, green: 0.339, blue: 0.342)
+                .edgesIgnoringSafeArea(.all)
+                VStack {
+                    List {
+                        ForEach(viewModel.todoItems) { item in
+                            HStack {
+                                Button(action: {
+                                    viewModel.toggleTodoItemCompletion(todoItem: item)
+                                }) {
+                                    Image(systemName: item.isCompleted ? "checkmark.circle.fill" : "circle")
+                                        .foregroundColor(item.isCompleted ? .blue : .primary)
+                                }
+                                .buttonStyle(PlainButtonStyle()) // Disable the button highlighting effect
+                                
+                                Text(item.title)
+                                    .strikethrough(item.isCompleted)
+                                
+                                Spacer()
+                                
+                                Button(action: {
+                                    if let index = viewModel.todoItems.firstIndex(where: { $0.id == item.id }) {
+                                        viewModel.todoItems.remove(at: index)
+                                    }
+                                }) {
+                                    Image(systemName: "trash.circle.fill")
+                                        .foregroundColor(.red)
+                                }
                             }
                         }
+                        .onDelete(perform: viewModel.deleteTodoItem)
+                        .background(.blue)
+                        
+                            
                     }
+                    .listStyle(.plain)
+                    
+                    
+                    
+                    HStack {
+                        TextField("new item", text: $newItemTitle)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Button(action: {
+                            viewModel.addTodoItem(title: newItemTitle)
+                            newItemTitle = ""
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                        }
+                        .foregroundColor(Color(red: 0.738, green: 0.64, blue: 0.676))
+                    }
+                    .padding()
                 }
+                
+                
+                navigationTitle("todo list")
             }
-        }
-    }
-
-    private func toggleItem(_ item: TodoItem) {
-        if let index = todoItems.firstIndex(where: { $0.id == item.id }) {
-            todoItems[index].isChecked.toggle()
-        }
-    }
-
-    private func deleteItem(_ item: TodoItem) {
-        if let index = todoItems.firstIndex(where: { $0.id == item.id }) {
-            todoItems[index].isDeleted = true
         }
     }
 }
@@ -67,4 +97,5 @@ struct todolist_Previews: PreviewProvider {
         todolist()
     }
 }
+
 
